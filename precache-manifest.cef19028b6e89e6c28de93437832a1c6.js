6c056bae0f9f36e6869409238720a37ce04d76c9
YAMF <1072664626@qq.com>
2021-03-22 21:24:54 +0800

    deploy

---
+++ precache-manifest.cef19028b6e89e6c28de93437832a1c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacb5590a4b1404e8a48",
+    "url": "/taner-website/css/app.eaf943ab.css"
+  },
+  {
+    "revision": "ec1547e64c4f004d49107eef86e1d486",
+    "url": "/taner-website/index.html"
+  },
+  {
+    "revision": "dacb5590a4b1404e8a48",
+    "url": "/taner-website/js/app.19df83c3.js"
+  },
+  {
+    "revision": "3725f990751196c3369d",
+    "url": "/taner-website/js/chunk-vendors.6a4495fe.js"
+  },
+  {
+    "revision": "7b248b1e9fcf3a0a5c2bdbd0b675cfd3",
+    "url": "/taner-website/manifest.json"
+  },
+  {
+    "revision": "1e1f4f2eb5e6e43dc5463d9cdf1c4868",
+    "url": "/taner-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/taner-website/robots.txt"
+  }
+]);
