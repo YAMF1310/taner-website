f1782d3f9623bc9fa45731cef330e8fa9c4a8dd7
YAMF1310 <1072664626@qq.com>
2021-12-23 12:01:53 +0800

    deploy

---
+++ precache-manifest.66b7598ded3c4f2b46f27d231db1faa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2643898dbbe8bfdffe",
+    "url": "/taner-website/css/app.eeaf157d.css"
+  },
+  {
+    "revision": "bad1d687089082977af2821f6a3add3f",
+    "url": "/taner-website/index.html"
+  },
+  {
+    "revision": "bb2643898dbbe8bfdffe",
+    "url": "/taner-website/js/app.f33d9549.js"
+  },
+  {
+    "revision": "3725f990751196c3369d",
+    "url": "/taner-website/js/chunk-vendors.6a4495fe.js"
+  },
+  {
+    "revision": "7b248b1e9fcf3a0a5c2bdbd0b675cfd3",
+    "url": "/taner-website/manifest.json"
+  },
+  {
+    "revision": "1e1f4f2eb5e6e43dc5463d9cdf1c4868",
+    "url": "/taner-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/taner-website/robots.txt"
+  }
+]);
